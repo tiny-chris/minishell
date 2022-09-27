4a8b9d4a64ba4b6cfb6eace5f27e094c1637c113
Lena MELARD <lmelard@e2r8p5.clusters.42paris.fr>
2022-09-27 14:29:47 +0200

    Fixing issue when modifying PWD= and OLDPWD= in data->env : absolute path

--- srcs/built_in/cd.c
+++ srcs/built_in/cd.c
@@ -6,7 +6,7 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 16:32:11 by cgaillag          #+#    #+#             */
-/*   Updated: 2022/09/27 12:34:22 by lmelard          ###   ########.fr       */
+/*   Updated: 2022/09/27 14:26:20 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 /*	mettre à jour OLDPWD et PWD et data->cwd
 
 	1 - nb arg: syntax
-	2 - opendir :
+	2 - access
+	3 - opendir :
 		int et si ok close
-	3 - access
 	4 - chdir
 */
 
@@ -29,26 +29,23 @@ void	ft_update_pwd(t_cmd *cmd, t_data *data)
 
 	env = data->env;
 	token = cmd->token->next;
+	oldpwd = NULL;
 	while (env)
 	{
 		if (ft_strncmp(env->var_equal, "PWD=", ft_strlen(env->var_equal)) == 0)
 		{	
 			oldpwd = ft_strdup(env->content);
 			if (!oldpwd)
-				return ;//free malloc !!
+				return ; //free malloc!!
 			free(env->content);
-			if (token)
-			{
-				env->content = ft_strdup(token->token);
-				if (!env->content)
-					return ;//free malloc !!
-			}
-			else
-			{
-				env->content = ft_strdup("/mnt/nfs/homes/lmelard/");
-				if (!env->content)
-					return ;//free malloc !!
-			}
+			env->content = NULL;
+			env->content = getcwd(NULL, 0);
+			if (!env->content)
+				return ; //free malloc!!
+			free(env->envp);
+			env->envp = ft_strjoin(env->var_equal, env->content);
+			if (!env->envp)
+				return ;//free malloc !!
 			break ;
 		}
 		env = env->next;
@@ -59,25 +56,26 @@ void	ft_update_pwd(t_cmd *cmd, t_data *data)
 		if (ft_strncmp(env->var_equal, "OLDPWD=", ft_strlen(env->var_equal)) == 0)
 		{	
 			free(env->content);
-			env->content = oldpwd;
+			env->content = ft_strdup(oldpwd);
+			free(env->envp);
+			env->envp = ft_strjoin(env->var_equal, env->content);
+			if (!env->envp)
+				return ; //free malloc !!
 			break ;
 		}
 		env = env->next;
 	}
+	if (oldpwd)
+		free(oldpwd);
 	if (data->oldpwd)
 		free(data->oldpwd);
 	data->oldpwd = ft_strdup(data->cwd);
 	if (!data->oldpwd)
 		return ;//free malloc !!
 	free(data->cwd);
-	if (token)
-	{
-		data->cwd = ft_strdup(token->token);
-		if (!data->cwd)
-			return ;//free malloc !!
-	}
-	else
-		data->cwd = ft_strdup("/mnt/nfs/homes/lmelard/");
+	data->cwd = getcwd(NULL, 0);
+	if (!data->cwd)
+		return ;//free malloc !!
 }
 
 int	ft_cd(t_cmd *cmd, t_data *data)
