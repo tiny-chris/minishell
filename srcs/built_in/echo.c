ae902894633e14218dd2f8c27d667b7d00c34818
Lena MELARD <lmelard@e3r7p20.clusters.42paris.fr>
2022-10-17 17:26:40 +0200

    Norm srcs/built_in/echo.c = OK

--- srcs/built_in/echo.c
+++ srcs/built_in/echo.c
@@ -3,77 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgaillag <cgaillag@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 10:53:43 by lmelard           #+#    #+#             */
-/*   Updated: 2022/10/13 16:14:16 by cgaillag         ###   ########.fr       */
+/*   Updated: 2022/10/17 17:23:54 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	ft_print_no_word_n(t_token *token, t_data *data, int flag)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	if (token->type == SP_QUOTES)
+	{
+		ft_handle_malloc(DELONE, token->token, 0, NULL);
+		token->token = ft_strdup("\n");
+		ft_handle_malloc(flag + TAB_STR1, token->token, 0, data);
+	}
+	else
+	{
+		tmp = ft_strdup(token->token);
+		ft_handle_malloc(flag + TAB_STR1, tmp, 0, data);
+		ft_handle_malloc(DELONE, token->token, 0, NULL);
+		token->token = ft_strjoin(tmp, "\n");
+		ft_handle_malloc(flag + TAB_STR1, token->token, 0, data);
+		ft_handle_malloc(DELONE, tmp, 0, NULL);
+	}
+	printf("%s", token->token);
+	return (0);
+}
+
 int	ft_echo(t_cmd *cmd, t_data *data, int flag)
 {
 	t_token	*token;
-	char	*tmp;
 
-	(void)data;
 	token = cmd->token;
 	token = token->next;
 	if (token)
 	{
 		if (token->type != WORD_N)
-		{
-			if (token->type == SP_QUOTES)
-			{
-				ft_handle_malloc(DELONE, token->token, 0, NULL);
-				// free(token->token);
-				token->token = ft_strdup("\n");
-				ft_handle_malloc(flag + TAB_STR1, token->token, 0, data);
-				// if (!token->token)
-				// 	return (ft_msg(1, "", "", ERRMAL));
-			}
-			else
-			{
-				tmp = ft_strdup(token->token);
-				ft_handle_malloc(flag + TAB_STR1, tmp, 0, data);
-				// if (!tmp)
-				// 	return (ft_msg(1, "", "", ERRMAL)); // FREEE + EXIT
-				ft_handle_malloc(DELONE, token->token, 0, NULL);
-				// free(token->token);
-				token->token = ft_strjoin(tmp, "\n");
-				ft_handle_malloc(flag + TAB_STR1, token->token, 0, data);
-				// if (!token->token)
-				// {
-				// 	free(tmp);
-				// 	return (ft_msg(1, "", "", ERRMAL));
-				// }
-				ft_handle_malloc(DELONE, tmp, 0, NULL);
-				// free(tmp);
-			}
-			printf("%s", token->token);
-			//ft_putstr_fd(token->token, STDOUT_FILENO);
-			return (0);
-		}
+			return (ft_print_no_word_n(token, data, flag));
 		if (token->next == NULL)
-		// {
 			return (write(1, "", 0));
-			// printf("");
-			// return (0);
-		// }
-		else//if (token->next)
+		else
 		{
 			if (token->next->type == SP_QUOTES)
 			{
 				ft_handle_malloc(DELONE, token->next->token, 0, NULL);
-				// free(token->next->token);
 				token->next->token = ft_strdup("");
 				ft_handle_malloc(flag + TAB_STR1, token->next->token, 0, data);
-				// if (!token->next->token)
-				// 	return (ft_msg(1, "", "", ERRMAL));
 			}
 			printf("%s", token->next->token);
-			//ft_putstr_fd(token->next->token, STDOUT_FILENO);
 			return (0);
 		}
 	}
