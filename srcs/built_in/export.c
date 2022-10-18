8c07f67f6248c6a2be7e7316aa7a72962ab37e50
Lena MELARD <lmelard@e3r7p21.clusters.42paris.fr>
2022-10-18 13:04:11 +0200

    Norm builtin Export OK

--- srcs/built_in/export.c
+++ srcs/built_in/export.c
@@ -6,28 +6,38 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 16:42:34 by cgaillag          #+#    #+#             */
-/*   Updated: 2022/10/17 18:59:47 by lmelard          ###   ########.fr       */
+/*   Updated: 2022/10/18 13:02:40 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*	GROS CHECK :
-		si le char * de token (texte jusqu'au '=') est équiv à un des env->var
-			alors : on remplace env->var, content... par les data de token
-		sinon (pas dans env) on crée le maillon env contenant les infos token
-			et on l'ajoute à la liste chainee t_env env
-		si la modification concerne la var PATH
-			alors on met t_env env_path à jour
+static void	ft_is_in_env(t_env *env, t_token *token, t_data *data, int flag)
+{
+	int		i;
+	size_t	size_content;
 
-		PROPOSITION:
-			--> util de ft_strncmp
-			--> si PATH= ==> free le env_path et rappeler la fonction ft_get_env_path
-			/!\ pour ft_get_env_path : MODIFIER ET UTILISER LA LISTE CHAINEE ENV AU LIEU DE CHAR *ENVP
-			/!\ récupérer s_env_path dans ft_get_env_path (= tab_path)
-				--> cela évite une redite (fonction 'ft_get_str_env_path' inutile)
-					et permet de mettre à jour en même temps s_env_path
-*/
+	i = ft_new_strchr(token->token, '=');
+	size_content = ft_strlen(token->token) - i + 1;
+	ft_handle_malloc(DELONE, env->content, 0, NULL);
+	env->content = ft_substr(token->token, i + 1, size_content);
+	ft_handle_malloc(flag + TAB_STR1, env->content, 0, data);
+	ft_handle_malloc(DELONE, env->envp, 0, NULL);
+	env->envp = ft_strdup(token->token);
+	ft_handle_malloc(flag + TAB_STR1, env->envp, 0, data);
+}
+
+static void	ft_export_path_home(t_env *env, t_data *data, int flag)
+{
+	size_t	size_var_equal;
+
+	size_var_equal = ft_strlen(env->var_equal);
+	if (ft_strncmp(env->var_equal, "PATH=", size_var_equal) == 0)
+		ft_get_env_path(data, flag);
+	else if (ft_strncmp(env->var_equal, "HOME=", size_var_equal) == 0)
+		ft_get_home(data, flag);
+}
+
 int	ft_check_export(t_token *token, t_data *data, int flag)
 {
 	t_env	*env;
@@ -43,17 +53,9 @@ int	ft_check_export(t_token *token, t_data *data, int flag)
 	{
 		if (ft_strncmp(env->var_equal, var_tmp, ft_strlen(env->var_equal)) == 0)
 		{
-			ft_handle_malloc(DELONE, env->content, 0, NULL);
-			env->content = ft_substr(token->token, i + 1, (ft_strlen(token->token) - i + 1));
-			ft_handle_malloc(flag + TAB_STR1, env->content, 0, data);
-			ft_handle_malloc(DELONE, env->envp, 0, NULL);
-			env->envp = ft_strdup(token->token);
-			ft_handle_malloc(flag + TAB_STR1, env->envp, 0, data);
+			ft_is_in_env(env, token, data, flag);
 			ft_handle_malloc(DELONE, var_tmp, 0, NULL);
-			if (ft_strncmp(env->var_equal, "PATH=", ft_strlen(env->var_equal)) == 0)
-				ft_get_env_path(data, flag);
-			else if (ft_strncmp(env->var_equal, "HOME=", ft_strlen(env->var_equal)) == 0)
-				ft_get_home(data, flag);
+			ft_export_path_home(env, data, flag);
 			return (0);
 		}
 		env = env->next;
@@ -65,31 +67,7 @@ int	ft_check_export(t_token *token, t_data *data, int flag)
 	return (0);
 }
 
-/*	***** version simplifiée *****
-	s'il n'y a pas de token après export
-		alors : erreur --> message ERRFEW
-	sinon on avance au token suivant
-	boucle tant que le token existe
-	1.	si 1er char n'est pas alpha ou '_' (val ASCII 95)
-			alors : erreur --> message ERRNAM
-	2.	sinon (1er char est un alpha ou '_')
-			alors :	tant que char + 1 existe  ET  char + 1 différent de '=' (boucle)
-						alors :	si char n'est pas alpha num ou '_'
-									alors : erreur --> message ERRNAM
-								sinon on avance (jusqu'à la fin du token)
-					si le char suivant (hors boucle) est un '='
-					alors : GROS CHECK
-						si le char * de token (texte jusqu'au '=') est équiv à un des env->var
-							alors : on remplace env->var, content... par les data de token
-						sinon (pas dans env) on crée le maillon env contenant les infos token
-							et on l'ajoute à la liste chainee t_env env
-						si la modification concerne la var PATH
-							alors on met t_env env_path à jour
-	3. on met à jour val_exit
-		et on change de token
-*/
-
-static int	ft_get_export_var(t_data *data, t_token *token, int *res, int flag)
+static int	ft_get_export(t_data *data, t_token *token, int *res, int flag)
 {
 	int	i;
 
@@ -131,7 +109,7 @@ int	ft_export(t_cmd *cmd, t_data *data, int flag)
 		else if ((token->token[0] == '_') && (token->token[1] == '='))
 			res = 0;
 		else
-			ft_get_export_var(data, token, &res, flag);
+			ft_get_export(data, token, &res, flag);
 		if (res == 0 && res2 == 0)
 			res2 = 0;
 		else
