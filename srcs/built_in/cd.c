07324d34b240ffd43ab102e8b1cbe74735f5e193
Lena MELARD <lmelard@e3r7p21.clusters.42paris.fr>
2022-10-18 18:43:45 +0200

    new function ft_oldpwd_pwd

--- srcs/built_in/cd.c
+++ srcs/built_in/cd.c
@@ -6,7 +6,7 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 16:32:11 by cgaillag          #+#    #+#             */
-/*   Updated: 2022/10/18 18:40:56 by lmelard          ###   ########.fr       */
+/*   Updated: 2022/10/18 18:43:01 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,6 +98,28 @@ static void	ft_oldpwd_nopwd(t_env *env, t_data *data, int flag)
 	}
 }
 
+static void	ft_oldpwd_pwd(t_env *env, t_data *data, int flag)
+{
+	if (!data->tmp_oldpwd)
+	{
+		ft_handle_malloc(DELONE, env->content, 0, NULL);
+		if (env->envp)
+			ft_handle_malloc(DELONE, env->envp, 0, NULL);
+		env->envp = ft_strdup("OLDPWD=");
+		ft_handle_malloc(flag + TAB_STR1, env->envp, 0, data);
+	}
+	else
+	{
+		ft_handle_malloc(DELONE, env->content, 0, NULL);
+		env->content = ft_strdup(data->tmp_oldpwd);
+		ft_handle_malloc(flag + TAB_STR1, env->content, 0, data);
+		if (env->envp)
+			ft_handle_malloc(DELONE, env->envp, 0, NULL);
+		env->envp = ft_strjoin(env->var_equal, env->content);
+		ft_handle_malloc(flag + TAB_STR1, env->envp, 0, data);
+	}
+}
+
 static void	ft_new_oldpwd(t_env *env, t_data *data, int flag, int pwd_null)
 {
 	while (env)
@@ -108,26 +130,7 @@ static void	ft_new_oldpwd(t_env *env, t_data *data, int flag, int pwd_null)
 			if (pwd_null == 1)
 				ft_oldpwd_nopwd(env, data, flag);
 			else
-			{
-				if (!data->tmp_oldpwd)
-				{
-					ft_handle_malloc(DELONE, env->content, 0, NULL);
-					if (env->envp)
-						ft_handle_malloc(DELONE, env->envp, 0, NULL);
-					env->envp = ft_strdup("OLDPWD=");
-					ft_handle_malloc(flag + TAB_STR1, env->envp, 0, data);
-				}
-				else
-				{
-					ft_handle_malloc(DELONE, env->content, 0, NULL);
-					env->content = ft_strdup(data->tmp_oldpwd);
-					ft_handle_malloc(flag + TAB_STR1, env->content, 0, data);
-					if (env->envp)
-						ft_handle_malloc(DELONE, env->envp, 0, NULL);
-					env->envp = ft_strjoin(env->var_equal, env->content);
-					ft_handle_malloc(flag + TAB_STR1, env->envp, 0, data);
-				}
-			}
+				ft_oldpwd_pwd(env, data, flag);
 			break ;
 		}
 		env = env->next;
