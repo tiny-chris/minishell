c8f26d08d7c0577e60737f1fb5eb2d906689649a
Lena MELARD <lmelard@e3r8p15.clusters.42paris.fr>
2022-10-19 21:37:40 +0200

    Norm parent_process.c = OK

--- srcs/exec/parent_process.c
+++ srcs/exec/parent_process.c
@@ -3,16 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   parent_process.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgaillag <cgaillag@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 17:28:34 by lmelard           #+#    #+#             */
-/*   Updated: 2022/10/16 19:10:53 by cgaillag         ###   ########.fr       */
+/*   Updated: 2022/10/19 21:35:42 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 extern int	g_val_exit;
+
+static void	ft_wifsignaled(int status)
+{
+	if (WTERMSIG(status) == SIGQUIT)
+	{
+		status = 128 + WTERMSIG(status);
+		ft_putstr_fd("Quit (core dumped)\n", 1);
+	}
+	else if (WTERMSIG(status) == SIGINT)
+	{
+		status = 128 + WTERMSIG(status);
+		write(1, "\n", 1);
+	}
+	else
+		status = EINTR;
+}
 
 int	ft_parent_process(t_data *data)
 {
@@ -28,25 +44,12 @@ int	ft_parent_process(t_data *data)
 	}
 	if (WIFEXITED(status))
 	{
-		if (status == 42)// A TESTER
+		if (status == 42)
 			ft_free_data_child(1, data);
 		status = WEXITSTATUS(status);
 	}
 	else if (WIFSIGNALED(status))
-	{
-		if (WTERMSIG(status) == SIGQUIT)
-		{
-			status = 128 + WTERMSIG(status);
-			ft_putstr_fd("Quit (core dumped)\n", 1);
-		}
-		else if (WTERMSIG(status) == SIGINT)
-		{
-			status = 128 + WTERMSIG(status);
-			write(1, "\n", 1);
-		}
-		else
-			status = EINTR;
-	}
+		ft_wifsignaled(status);
 	ft_clean_exec(data);
 	return (status);
 }
@@ -56,7 +59,6 @@ int	ft_parent_exit(t_data *data, int res)
 	int		i;
 	int		status;
 
-	// (void) res;
 	ft_close_fd(data);
 	i = 0;
 	ft_msg(g_val_exit, ERRMSG, "", strerror(g_val_exit));
