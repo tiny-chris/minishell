1d4ed2b16315cc222e796dd67f47f3a2f84c64ff
Lena MELARD <lmelard@e3r4p20.clusters.42paris.fr>
2022-10-22 00:06:43 +0200

    Fixing bug ctrl + C signal

--- srcs/exec/parent_process.c
+++ srcs/exec/parent_process.c
@@ -6,7 +6,7 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 17:28:34 by lmelard           #+#    #+#             */
-/*   Updated: 2022/10/19 21:35:42 by lmelard          ###   ########.fr       */
+/*   Updated: 2022/10/22 00:05:28 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 extern int	g_val_exit;
 
-static void	ft_wifsignaled(int status)
+static void	ft_wifsignaled(int *status)
 {
-	if (WTERMSIG(status) == SIGQUIT)
+	if (WTERMSIG(*status) == SIGQUIT)
 	{
-		status = 128 + WTERMSIG(status);
+		*status = 128 + WTERMSIG(*status);
 		ft_putstr_fd("Quit (core dumped)\n", 1);
 	}
-	else if (WTERMSIG(status) == SIGINT)
+	else if (WTERMSIG(*status) == SIGINT)
 	{
-		status = 128 + WTERMSIG(status);
+		*status = 128 + WTERMSIG(*status);
 		write(1, "\n", 1);
 	}
 	else
-		status = EINTR;
+		*status = EINTR;
 }
 
 int	ft_parent_process(t_data *data)
@@ -49,7 +49,7 @@ int	ft_parent_process(t_data *data)
 		status = WEXITSTATUS(status);
 	}
 	else if (WIFSIGNALED(status))
-		ft_wifsignaled(status);
+		ft_wifsignaled(&status);
 	ft_clean_exec(data);
 	return (status);
 }
