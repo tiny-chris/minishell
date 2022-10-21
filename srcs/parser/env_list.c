a667cdc393d5647e57726a4ae7ec6ea586f418d8
Christel GAILLAGOT <cgaillag@e3r5p20.clusters.42paris.fr>
2022-10-21 10:45:05 +0200

    parser - env_list.c = NORM OK

--- srcs/parser/env_list.c
+++ srcs/parser/env_list.c
@@ -26,9 +26,6 @@ t_env	*ft_lstlast_env(t_env *lst)
 	return (node);
 }
 
-//flag = ADD_M ou ADD_C
-// ajout de head_addr pour nettoyer dans t_env via l'appel de t_bin (pas d eliste doublement chainee)
-
 int	ft_lstadd_env(t_env **env, char *envp, t_data *data, int flag)
 {
 	t_env	*new;
@@ -36,10 +33,7 @@ int	ft_lstadd_env(t_env **env, char *envp, t_data *data, int flag)
 	int		equal;
 
 	new = ft_handle_malloc(flag + 1 + LST_ENV, NULL, 1, data);
-	// printf("test ici 1\n");
 	equal = ft_new_strchr(envp, '=');
-	// QUID si equal == NULL ?
-	// printf("test ici 2\n");
 	new->var = ft_substr(envp, 0, equal);
 	ft_handle_malloc(flag + TAB_STR1, new->var, 0, data);
 	new->var_equal = ft_substr(envp, 0, equal + 1);
@@ -49,7 +43,6 @@ int	ft_lstadd_env(t_env **env, char *envp, t_data *data, int flag)
 	new->envp = ft_strdup(envp);
 	ft_handle_malloc(flag + TAB_STR1, new->envp, 0, data);
 	new->next = NULL;
-	// printf("test ici 3\n");
 	if (ft_lstlast_env(*env) == 0)
 	{
 		*env = new;
@@ -57,60 +50,8 @@ int	ft_lstadd_env(t_env **env, char *envp, t_data *data, int flag)
 	}
 	last = ft_lstlast_env(*env);
 	last->next = new;
-	// new->head_addr = (*env);
 	return (0);
 }
-
-// // OLD VERSION - WITH NO ft_handle_malloc
-// int	ft_lstadd_env(t_env **env, char *envp)
-// {
-// 	t_env	*new;
-// 	t_env	*last;
-// 	int		equal;
-// 	int		len;
-
-// 	new = malloc(sizeof(t_env));
-// 	if (!new)
-// 	{
-// 		ft_free_env(env);
-// 		return (1);
-// 	}
-// 	equal = ft_new_strchr(envp, '=');
-// 	len = ft_strlen(envp);
-// 	printf("equal = %d\n", equal);
-// 	// si equal == NULL ?
-// 	new->var = ft_substr(envp, 0, equal);
-//	new->var_equal = ft_substr(envp, 0, equal + 1);
-// 	new->content = ft_substr(envp, equal + 1, (len - equal + 1));
-// 	new->envp = ft_strdup(envp);
-// 	new->next = NULL;
-// 	if (ft_lstlast_env(*env) == 0)
-// 	{
-// 		*env = new;
-// 		return (0);
-// 	}
-// 	last = ft_lstlast_env(*env);
-// 	last->next = new;
-// 	return (0);
-// }
-
-// void	ft_lstdelone_env(t_env *node, int flag)
-// {
-// 	if (!node)
-// 		return ;
-// 	(void) flag;
-// 	// if (flag == DELONE)
-// 	// {
-// 	// 	ft_free_strs(node->var, node->var_equal, node->content);
-// 	// //	dprintf(2, "ca bug pas là\n");
-// 	// 	ft_free_strs(node->envp, NULL, NULL);
-// 	// // free(node->head_addr);
-// 	// // node->head_addr = NULL;
-// 	// }
-// 	node->next = NULL;
-// 	free(node);
-// 	node = NULL;
-// }
 
 void	ft_lstdelone_env(t_env *node)
 {
@@ -133,8 +74,6 @@ void	ft_lstdelone_env_bin(t_env *node)
 		ft_handle_malloc(DELONE, node->content, 0, NULL);
 	if (node->envp)
 		ft_handle_malloc(DELONE, node->envp, 0, NULL);
-	// if (node->head_addr)// A REVOIR !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// 	ft_handle_malloc(DELONE, node->head_addr, 0, 0);
 	ft_handle_malloc(DELONE, node, 0, NULL);
 }
 
@@ -156,54 +95,3 @@ void	ft_free_env(t_env **env)
 	}
 	(*env) = NULL;
 }
-
-// A VOIR SI ON LES UTILISE...
-
-// static int	ft_lstclearfirst_env(t_env **env_head)
-// {
-// 	t_env	*todel;
-// 	t_env	*tmp;
-
-// 	todel = (*env_head);
-// 	tmp = (*env_head)->next;
-// 	ft_handle_malloc(DELONE, todel, 0, NULL);
-// 	ft_lstdelone_env(todel);
-// 	(*env_head) = tmp;
-// 	return (0);
-// }
-
-// void	ft_lstclearone_env(t_env **env_head, t_env *ptr)
-// {
-// //	t_env	**env_head;
-// 	t_env	*env;
-// 	t_env	*todel;
-// 	t_env	*tmp;
-
-// 	if (env_head == NULL)
-// 		(*env_head) = ptr->head_addr;
-// 	if (ptr->head_addr == NULL)
-// 		return ;
-// 	env = (*env_head);
-// 	todel = NULL;
-// 	tmp = NULL;
-// 	if (!(*env_head) || !ptr)
-// 		return ;
-// 	if (env == ptr)
-// 	{
-// 		ft_lstclearfirst_env(env_head);
-// 		return ;
-// 	}
-// 	while (env != NULL)
-// 	{
-// 		if (env->next && env->next == ptr)
-// 		{
-// 			todel = env->next;
-// 			tmp = env->next->next;
-// 			ft_handle_malloc(DELONE, todel, 0, NULL);
-// 			ft_lstdelone_env(todel);// corriger/checker le flag ///
-// 			env->next = tmp;
-// 			break ;
-// 		}
-// 		env = env->next;
-// 	}
-// }
