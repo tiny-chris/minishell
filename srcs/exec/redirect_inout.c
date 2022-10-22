98a24747700545c7a2a306c366dc5e31356c1716
Lena MELARD <lmelard@e3r7p20.clusters.42paris.fr>
2022-10-22 15:37:49 +0200

    Fixing fd leaks when redir failed

--- srcs/exec/redirect_inout.c
+++ srcs/exec/redirect_inout.c
@@ -6,7 +6,7 @@
 /*   By: lmelard <lmelard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 22:40:01 by lmelard           #+#    #+#             */
-/*   Updated: 2022/10/18 22:58:06 by lmelard          ###   ########.fr       */
+/*   Updated: 2022/10/22 15:33:20 by lmelard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,20 +99,23 @@ static int	ft_redirect_middle_cmd(t_data *data, t_cmd *cmd, int i)
 
 int	ft_redirect_inout(t_data *data, t_cmd *cmd, int i)
 {
-	if (i == 0)
+	if (cmd->file_err != 1)
 	{
-		if (ft_redirect_first_cmd(data, cmd, i) == -1)
-			return (-1);
-	}
-	else if (i > 0 && i == data->nb_pipes)
-	{
-		if (ft_redirect_last_cmd(data, cmd, i) == -1)
-			return (-1);
-	}
-	else
-	{
-		if (ft_redirect_middle_cmd(data, cmd, i) == -1)
-			return (-1);
+		if (i == 0)
+		{
+			if (ft_redirect_first_cmd(data, cmd, i) == -1)
+				return (-1);
+		}
+		else if (i > 0 && i == data->nb_pipes)
+		{
+			if (ft_redirect_last_cmd(data, cmd, i) == -1)
+				return (-1);
+		}
+		else
+		{
+			if (ft_redirect_middle_cmd(data, cmd, i) == -1)
+				return (-1);
+		}
 	}
 	return (0);
 }
